99b605088d6f8117ad41f9b268fdf76c15e43288
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2023-08-11 23:36:26 -0700

    Initial

---
+++ shaderdoc.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+from typing import TypedDict, List
+from io import TextIOWrapper
+import os
+
+omitted_example = ['$flags', '$flags_defined', '$flags2', '$flags_defined2']
+omitted = ['$flags_defined', '$flags_defined2']
+
+class ShaderParamType(TypedDict):
+    name: str
+    desc: str
+    type: str
+    default: str
+
+class ShaderType(TypedDict):
+    name: str
+    params: List[ShaderParamType]
+
+
+class Shader:
+    def __init__(self, desc: ShaderType):
+        self.params = desc['params']
+        self.name = desc['name']
+    
+
+    def _emit_param(self, param: ShaderParamType, stream: TextIOWrapper) -> None:
+        if param['name'] in omitted:
+            return
+        stream.write(f'### `{param["name"].lower()}` \\<{param["type"]}\\>\n\n')
+        stream.write(f'Default: `{param["default"]}`\n\n')
+        stream.write(f'{param["desc"]}\n\n')
+        
+
+    def _emit_example(self, stream: TextIOWrapper) -> None:
+        stream.write(f'```\n{self.name}\n{{\n')
+        for p in self.params:
+            if p['name'] in omitted_example:
+                continue
+            stream.write(f'\t{p["name"].lower():20} "{p["default"]}"\n')
+        stream.write("}\n```\n\n")
+
+
+    def _emit_params(self, stream: TextIOWrapper) -> None:
+        for param in self.params:
+            self._emit_param(param, stream)
+        
+
+    def emit_docs(self, stream: TextIOWrapper) -> None:
+        stream.write(f'# {self.name}\n\n## Parameters\n\n')
+        self._emit_params(stream)
+        stream.write('## Example\n\n')
+        self._emit_example(stream)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True, help='File to generate off of')
+    parser.add_argument('-o', required=True, help='Directory to output data to')
+    args = parser.parse_args()
+
+    j = {}
+    with open(args.i, 'rb') as fp:
+        j = json.load(fp)
+    
+    try:
+        os.makedirs(args.o)
+    except:
+        pass
+
+    shaders = []
+    for s in j:
+        shader = Shader(s)
+        with open(f'{args.o}/{shader.name}.md', 'w') as fp:
+            shader.emit_docs(fp)
